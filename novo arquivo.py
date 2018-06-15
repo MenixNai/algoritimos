678d1f79ee74bf62118f772a5df5454ead202b6e
Aluno <menixnai@gmail.com>
2018-06-14 22:33:59 -0300

    novo arquivo.py

--- novo arquivo.py
+++ novo arquivo.py
@@ -14,17 +14,35 @@
 #     p+=1
 # print(sp/peso)
 
-t = int(input("Qual vai ser o tamanho dessa lista: "))
+# t = int(input("Qual vai ser o tamanho dessa lista: "))
+#
+# lista = ['']*t
+# p=0
+# r=0
+# sp=0
+# si=0
+# while p<len(lista):
+#     lista[p] = p
+#     p=p+1
+# p=0
+# for valor in lista:
+#     print("Valor amarzenado na local {} é {}".format(p,valor*2))
+#     p=p+1
 
-lista = ['']*t
+
+lista = ['']*6
 p=0
-r=0
-sp=0
-si=0
+
 while p<len(lista):
-    lista[p] = int(input("Digite valor: "))
+    lista[p] = input("Qual numero: ")
     p=p+1
 p=0
-for valor in lista:
-    print("Valor amarzenado na local {} é {}".format(p,valor))
+aposta = ['']*6
+while p<len(lista):
+    lista[p] = input("Qual numero sorteado: ")
     p=p+1
+p=0
+s=0
+while p<len(lista):
+    if lista[p] == lista[s]
+    lista[s]=
